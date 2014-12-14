5af037adc384227411eee9276ced6f031b391dd3
liuzhenkun <liujieke@126.com>
2014-12-14 22:59:59 +0800

    Chat0.1首次提交

---
+++ src/ChatClient.java
@@ -0,0 +1,14 @@
+import java.awt.Frame;
+
+
+public class ChatClient extends Frame{
+	public static void main(String[] args) {
+		new ChatClient().launchFrame();
+	}
+	public void launchFrame(){
+		this.setLocation(400,300);
+		this.setSize(300,300);
+		setVisible(true);
+		
+	}
+}
